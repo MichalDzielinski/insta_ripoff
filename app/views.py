27b50a8c2b76542d445249490077e2b4e0e3559a
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-04 06:09:31 +0200

    next one

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def gallery(request):
     return render(request, 'gallery.html')
 
-def viewPhoto(request):
+def viewPhoto(request, pk):
     return render(request, 'view.html')
 
 def addPhoto(request):
